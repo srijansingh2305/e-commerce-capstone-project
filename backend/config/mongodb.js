8a419bc132d4dfb70ac7e4c91dc00f29bf9caf69
Srijan <srijansingh235@gmail.com>
2024-12-17 02:09:34 +0530

    await mongoose.connect('mongodb://127.0.0.1:27017/e-commerce');

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -16,7 +16,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`);
+        await mongoose.connect('mongodb://127.0.0.1:27017/e-commerce');
+
         console.log("DB Connected");
     } catch (error) {
         console.error("MongoDB Connection Error:", error.message);
